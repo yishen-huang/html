5b20f9dd62c600f819472088c9a26b18e89cb271
clackypaas <clackypaas>
2024-10-25 18:54:13 +0800

    feat: add hamburger section with recipe details to the webpage

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-
 function openModal() {
     var modal = document.getElementById('recipeModal');
     modal.style.display = 'block';
     setTimeout(function() {
         modal.style.display = 'none';
-    }, 10000);
+    }, 5000);
+}
+
+function openHamburgerModal() {
+    var modal = document.getElementById('hamburgerModal');
+    modal.style.display = 'block';
+    setTimeout(function() {
+        modal.style.display = 'none';
+    }, 5000);
 }
